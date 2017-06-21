04bf678d7250501296b74ec81848707e7ba66ba1
fcremo (Filippo Cremonese) <fcremo@users.noreply.github.com>
2017-06-21 17:56:54 +0200

    Fixed test coverage run configuration, wrote a new test

--- src/test/java/ing/polimi/ingsw/EffectDeserializerTest.java
+++ src/test/java/ing/polimi/ingsw/EffectDeserializerTest.java
@@ -2,22 +2,69 @@ package ing.polimi.ingsw;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import model.board.Board;
+import model.card.development.BuildingCard;
+import model.card.effects.EffectsContainer;
+import model.card.effects.ImmediateResourcesEffect;
 import model.card.effects.ObtainedResourceSetModifierEffect;
 import model.card.effects.interfaces.EffectInterface;
+import model.resource.ObtainedResourceSet;
+import model.resource.RequiredResourceSet;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import server.configloader.deserializers.EffectDeserializer;
+import server.configloader.deserializers.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 
 public class EffectDeserializerTest {
-    @Test
-    public void testObtainedResourceSetDeserialization(){
-        // TODO: 5/26/17 move test cases to separate files
-        final String testCase = "{\"effectType\": \"ObtainedResourceSetModifier\", \"resourceType\": \"MILITARY_POINTS\", \"quantity\": -1}";
-        Gson gson = new GsonBuilder().registerTypeAdapter(EffectInterface.class, new EffectDeserializer()).create();
-        EffectInterface effect = gson.fromJson(testCase, EffectInterface.class);
+    private static Gson deserializer;
 
-        assertTrue(ObtainedResourceSetModifierEffect.class.isAssignableFrom(effect.getClass()));
+    @BeforeClass
+    public static void setupObjects() {
+        deserializer = new GsonBuilder()
+                .registerTypeAdapter(EffectInterface.class, new EffectDeserializer())
+                .registerTypeAdapter(ObtainedResourceSet.class, new ObtainedResourceSetDeserializer())
+                .registerTypeAdapter(RequiredResourceSet.class, new RequiredResourceSetDeserializer())
+                .create();
+    }
+
+    @Test
+    public void testImmediateResourcesEffectDeserialization() {
+        final String testCase = "{\"effectType\": \"ImmediateResources\", \"resources\": {\"VICTORY_POINTS\": 5}}";
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .registerTypeAdapter(Board.class, new BoardDeserializer())
+                .registerTypeAdapter(ObtainedResourceSet.class, new ObtainedResourceSetDeserializer())
+                .registerTypeAdapter(RequiredResourceSet.class, new RequiredResourceSetDeserializer())
+                .registerTypeAdapter(BuildingCard.class, new BuildingCardDeserializer())
+                .registerTypeAdapter(EffectsContainer.class, new EffectsContainerDeserializer())
+                .registerTypeAdapter(EffectInterface.class, new EffectDeserializer())
+                .create();
+
+        ImmediateResourcesEffect effect = deserializeEffect(testCase);
+        assertTrue(effect.getObtainedResourceSet().equals(new ObtainedResourceSet(0, 0, 0, 0, 0, 0, 0,5)));
+    }
+
+
+    @Test
+    public void testObtainedResourceSetDeserialization() {
+        final String testCase = "{\"effectType\": \"ObtainedResourceSetModifier\", \"resourceType\": \"MILITARY_POINTS\", \"quantity\": -1}";
+        ObtainedResourceSetModifierEffect effect = deserializeEffect(testCase);
+
+        // TODO: test something
+    }
+
+    /**
+     * Tries to deserialize the provided string as a certain kind of effect
+     * @param serializedEffect
+     * @param <T extends EffectInterface> the kind of effect that should be deserialized
+     * @return
+     */
+    private <T extends EffectInterface> T deserializeEffect(String serializedEffect) {
+        EffectInterface deserializedEffect = deserializer.fromJson(serializedEffect, EffectInterface.class);
+
+        return (T)deserializedEffect;
     }
 }
