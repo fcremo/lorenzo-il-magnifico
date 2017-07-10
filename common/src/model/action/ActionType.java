29f6db15d2f802dfd715a5e971c7d1b2825708fb
giuliabee <giuliaboggiali@gmail.com>
2017-07-10 08:36:22 +0200

    removed underscore when printing action types

--- common/src/model/action/ActionType.java
+++ common/src/model/action/ActionType.java
@@ -1,13 +1,19 @@
 package model.action;
 
 public enum ActionType {
-    TAKE_ANY_CARD,
-    TAKE_BUILDING_CARD,
-    TAKE_TERRITORY_CARD,
-    TAKE_CHARACTER_CARD,
-    TAKE_VENTURE_CARD,
-    PRODUCTION,
-    HARVEST;
+    TAKE_ANY_CARD("TAKE ANY CARD"),
+    TAKE_BUILDING_CARD("TAKE BUILDING CARD"),
+    TAKE_TERRITORY_CARD("TAKE TERRITORY CARD"),
+    TAKE_CHARACTER_CARD("TAKE CHARACTER CARD"),
+    TAKE_VENTURE_CARD("TAKE VENTURE CARD"),
+    PRODUCTION("PRODUCTION"),
+    HARVEST("HARVEST");
+
+    String name;
+
+    ActionType(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
